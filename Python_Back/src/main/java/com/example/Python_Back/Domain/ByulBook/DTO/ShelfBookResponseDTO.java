545d7bfa1420d0a0b16b336d4b0b46415ea2f637
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-11-15 01:13:50 +0900

    상부삼조 백엔드 세 번째 커밋 - 사용자 서재 데이터 반환(다,덜,안 읽은 책)

---
+++ Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/DTO/ShelfBookResponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.Python_Back.Domain.ByulBook.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ShelfBookResponseDTO {
+    private String title;
+    private String coverImageUrl;
+}
---
+++ Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/DTO/ShelfBooksByStatusDTO.java
@@ -0,0 +1,17 @@
+package com.example.Python_Back.Domain.ByulBook.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ShelfBooksByStatusDTO {
+    private Long kakaoId; // 사용자 식별자
+    private List<ShelfBookResponseDTO> readBooks; // 다 읽은 책
+    private List<ShelfBookResponseDTO> partiallyReadBooks; // 덜 읽은 책
+    private List<ShelfBookResponseDTO> unreadBooks; // 안 읽은 책
+}
