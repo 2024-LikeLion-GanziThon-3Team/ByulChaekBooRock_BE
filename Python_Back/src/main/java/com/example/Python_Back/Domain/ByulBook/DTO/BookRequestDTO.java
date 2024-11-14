aa659ad34fe47a678bcca182b8792b241c1fcf6b
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-11-15 01:12:23 +0900

    상부삼조 백엔드 두 번째 커밋 - 추천받은 책 감상평 조회 기능 - 추천받은 책 서재에 추가 기능

--- Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/DTO/BookRequestDTO.java
+++ Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/DTO/BookRequestDTO.java
@@ -3,8 +3,10 @@ package com.example.Python_Back.Domain.ByulBook.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class BookRequestDTO {
-    private String title;
+    private List<String> titles;  // 책 제목 리스트
 }
--- Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/Entity/Shelf.java
+++ Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/Entity/Shelf.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,14 +14,23 @@ import java.util.ArrayList;
 public class Shelf {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long ShelfId;
+    private Long shelfId;  // 서재 고유 ID
 
     @OneToOne
     @JoinColumn(name = "kakaoId")
     private KakaoUser kakaoUser;  // 사용자와의 1:1 관계
 
-    // @OneToMany(mappedBy = "bookshelf", cascade = CascadeType.ALL)
-    // private List<BookshelfBook> books = new ArrayList<>();
+    @OneToMany(mappedBy = "shelf", cascade = CascadeType.ALL)
+    private List<ShelfBook> shelfBooks = new ArrayList<>();  // 서재에 있는 책 목록
+
+
+    // 기본 생성자
+    public Shelf() {}
+
+    // KakaoUser를 받는 생성자
+    public Shelf(KakaoUser kakaoUser) {
+        this.kakaoUser = kakaoUser;
+    }
 
 
 
