4d7fc3f66e0dab127d1b4a5120bb5ae69dc925cf
gahyeon <gimgahyeon12@gmail.com>
2024-11-17 18:59:32 +0900

    Feat: 책갈피 crud구현

---
+++ Python_Back/src/main/java/com/example/Python_Back/Domain/ByulBook/DTO/BookMarkDTO.java
@@ -0,0 +1,19 @@
+package com.example.Python_Back.Domain.ByulBook.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BookMarkDTO {
+    private Long bookmarkId;
+    private Long shelfBookId; // ShelfBook의 ID 추가
+    private Integer pageNumber;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
